f0604452c5f943463de915b44f86f4abce29a79c
yoshioka0101 <yuuki0205nano@gmail.com>
2025-03-03 23:34:30 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
+}
+
+func main() {
+	fmt.Println("Hello World")
+
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	app.Post("/api/todos", func (c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
